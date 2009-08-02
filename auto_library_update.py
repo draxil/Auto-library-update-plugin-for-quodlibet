8831148df051a12b72822813be689804006c7ee4
joe <x@zaphod.(none)>
2009-08-02 13:17:28 +0100

    basic working, but not good threadwise

--- auto_library_update.py
+++ auto_library_update.py
@@ -5,49 +5,63 @@ from pyinotify import WatchManager, ThreadedNotifier, EventsCodes, ProcessEvent
 
 from plugins.events import EventPlugin
 import config
-import library
 
 # Set this to True to enable logging
-verbose = True
+verbose = False
 
 class AutoLibraryUpdate ( EventPlugin ):
     
-    # tell QL about our plugin:
+    # Tell QL about our plugin:
     PLUGIN_ID = "Automatic library update"
     PLUGIN_NAME = "Automatic library update"
     PLUGIN_DESC = "Keep your quodlibet library up-to-date with inotify"
     PLUGIN_VERSION = "0.1"
     
+    # Set everything up:
     def __init__( self ):
-        log( 'Init' )
+        from player import playlist as player
+        from quodlibet.library import library as library
+        print library
+        self.library = library
 
         wm = WatchManager()
 
-        tn = ThreadedNotifier( wm, self.ALE())
+        event_handler = self.ALE( self )
+
+        tn = ThreadedNotifier( wm, event_handler )
         tn.daemon = True
 
         tn.start()
 
         FLAGS=EventsCodes.ALL_FLAGS
-        mask = FLAGS['IN_DELETE'] | FLAGS['IN_CREATE']  # watched events
+        mask = FLAGS['IN_DELETE'] | FLAGS['IN_CLOSE_WRITE']  # watched events
 
         # watch paths in scan_list:
         for path in self.scan_list():
             log ( 'Adding watch: for ' + path )
             wm.add_watch( path, mask, rec=True )
-
-
+    
+    # find list of directories to scan
     def scan_list(self):
         return config.get( "settings", "scan").split(":")
 
+    # auto-library-event class, handles the events
     class ALE( ProcessEvent ):
-        def process_IN_CREATE(self, event):
-            log( 'Create file event' )
-            library.scan( [ os.path.join( event.path, event.name ) ] )
+        def __init__( self, alu ):
+            self._alu = alu
         
+        # process close-write event
+        def process_IN_CLOSE_WRITE(self, event):
+            item =  self._alu.library.add_filename ( os.path.join(event.path, event.name) )
+            if item:
+                log( '%s added to library' % item )
+
+        # process delete event
         def process_IN_DELETE(self, event):
-            log( 'Delete file event' )
-            print "Remove: %s" %  os.path.join(event.path, event.name)    
+            item = self._alu.library.__getitem__( os.path.join(event.path, event.name) )
+            log( 'removing %s from library' % item )
+            if item:
+                self._alu.library.reload(item)
 
 
 def log(msg):
