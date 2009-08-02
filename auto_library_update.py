1c47e40fb6f10b4742d311acb93fc73708d7c145
joe <x@zaphod.(none)>
2009-08-02 13:32:30 +0100

    idle wrap 	old_lupd.py

--- auto_library_update.py
+++ auto_library_update.py
@@ -5,6 +5,7 @@ from pyinotify import WatchManager, ThreadedNotifier, EventsCodes, ProcessEvent
 
 from plugins.events import EventPlugin
 import config
+import glib
 
 # Set this to True to enable logging
 verbose = False
@@ -52,12 +53,19 @@ class AutoLibraryUpdate ( EventPlugin ):
         
         # process close-write event
         def process_IN_CLOSE_WRITE(self, event):
+            glib.idle_add( self.add_event, event )
+
+        def add_event ( self, event ):
             item =  self._alu.library.add_filename ( os.path.join(event.path, event.name) )
             if item:
                 log( '%s added to library' % item )
+            return False
 
         # process delete event
         def process_IN_DELETE(self, event):
+            glib.idle_add( self.process_event, event )
+        
+        def process_event( self, event ):
             item = self._alu.library.__getitem__( os.path.join(event.path, event.name) )
             log( 'removing %s from library' % item )
             if item:
