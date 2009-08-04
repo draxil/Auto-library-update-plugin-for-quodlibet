6cf869f9a004b2dc013236f44bf1df762d6fad1b
Joe Higton <joe@deepthought.(none)>
2009-08-04 08:57:55 +0100

    Support for move operations, some cleanup

--- auto_library_update.py
+++ auto_library_update.py
@@ -8,14 +8,14 @@ import config
 import glib
 
 # Set this to True to enable logging
-verbose = False
+verbose = True
 
 class AutoLibraryUpdate ( EventPlugin ):
     
     # Tell QL about our plugin:
     PLUGIN_ID = "Automatic library update"
     PLUGIN_NAME = "Automatic library update"
-    PLUGIN_DESC = "Keep your quodlibet library up-to-date with inotify"
+    PLUGIN_DESC = "Keep your quodlibet library up-to-date with inotify."
     PLUGIN_VERSION = "0.1"
     
     library = None
@@ -42,7 +42,10 @@ class AutoLibraryUpdate ( EventPlugin ):
             self.tn.start()
 
             FLAGS=EventsCodes.ALL_FLAGS
-            mask = FLAGS['IN_DELETE'] | FLAGS['IN_CLOSE_WRITE']  # watched events
+            
+            # mask for watched events:
+            mask = FLAGS['IN_DELETE'] | FLAGS['IN_CLOSE_WRITE']\
+                   | FLAGS['IN_MOVED_FROM'] | FLAGS['IN_MOVED_TO']
 
             # watch paths in scan_list:
             for path in self.scan_list():
@@ -64,12 +67,15 @@ class AutoLibraryUpdate ( EventPlugin ):
         def __init__( self, alu ):
             self._alu = alu
         
-        # process close-write event
+        # process close-write event (  copy, new file etc )
         def process_IN_CLOSE_WRITE(self, event):
-            if not self._alu.running: 
-                return
             glib.idle_add( self.add_event, event )
 
+        # process moved-to event:
+        def process_IN_MOVED_TO( self, event ):
+            glib.idle_add( self.add_event, event )
+
+        # general we think we added something callback:
         def add_event ( self, event ):
             item =  self._alu.library.add_filename ( os.path.join(event.path, event.name) )
             if item:
@@ -79,7 +85,12 @@ class AutoLibraryUpdate ( EventPlugin ):
         # process delete event
         def process_IN_DELETE(self, event):
             glib.idle_add( self.process_event, event )
+
+        # process the moved-from event:
+        def process_IN_MOVED_FROM( self, event ):
+            glib.idle_add( self.process_event, event )
         
+        # general "check to see if it's still there" callback:
         def process_event( self, event ):
             item = self._alu.library.__getitem__( os.path.join(event.path, event.name) )
             log( 'removing %s from library' % item )
